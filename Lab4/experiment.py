4e9f4ea61fe9940013ee1a2fa19d007908360a2d
Artem Yakubets <yakubets.artem05@gmail.com>
2025-05-28 00:18:47 +0200

    ugu

--- Lab4/experiment.py
+++ Lab4/experiment.py
@@ -41,3 +41,37 @@ if __name__ == '__main__':
     mc.cook_temp.view()
     plt.tight_layout()
     plt.show()
+
+
+
+    # for case in test_cases:
+    #     # Скидаємо попередній стан
+    #     mc.sim.reset()
+    #     # Призначаємо входи
+    #     mc.sim.input['volume']  = case['volume']
+    #     mc.sim.input['ratio']   = case['ratio']
+    #     mc.sim.input['temp_in'] = case['temp_in']
+    #     mc.sim.input['dish']    = case['dish']
+    #     # Обчислюємо
+    #     mc.sim.compute()
+
+    #     # Результати
+    #     time_pred, temp_pred = mc.sim.output['cook_time'], mc.sim.output['cook_temp']
+    #     print(
+    #         f"{labels[case['dish']]:12s} | "
+    #         f"vol={case['volume']:.1f}L, ratio={case['ratio']:.1f}, "
+    #         f"temp_in={case['temp_in']:.1f}°C -> "
+    #         f"cook_time={time_pred:.2f}min, cook_temp={temp_pred:.2f}°C"
+    #     )
+
+    #     # 1) Графік дефазифікації cook_time
+    #     mc.cook_time.view(sim=mc.sim)
+    #     plt.title(f"Defuzz cook_time for {labels[case['dish']]}")
+    #     plt.tight_layout()
+    #     plt.show()
+
+    #     # 2) Графік дефазифікації cook_temp
+    #     mc.cook_temp.view(sim=mc.sim)
+    #     plt.title(f"Defuzz cook_temp for {labels[case['dish']]}")
+    #     plt.tight_layout()
+    #     plt.show()
